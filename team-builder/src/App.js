fb9e10b2189abcec33757e95158909bb767caf29
unknown <Kat2bk@gmail.com>
2020-03-18 18:55:50 -0700

    set up form in form.js using useState and blank attributes to function component. imported form.js to App.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/form";
 
 function App() {
-  const [member, setMember] = useState([]);
+  const [members, setMembers] = useState([]);
 
+  const addNewMember = member => {
+    const newMember = {
+      id: Date.now(),
+      name: member.name,
+      email: member.email,
+      position: member.position
+    };
+    setMembers([...members, newMember]);
+  };
 
-  
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Teammate List</h1>
+      <h2>Add your name!</h2>
+      <Form data={addNewMember} />
+    </div>
+  );
 }
 
 export default App;
--- team-builder/src/components/form.js
+++ team-builder/src/components/form.js
@@ -1,1 +1,11 @@
 import React, { useState } from "react";
+
+export default function MemberForm(props) {
+  console.log("I am the props for form", props);
+  const [formState, setFormState] = useState({
+    id: "",
+    name: "",
+    email: "",
+    position: ""
+  });
+}
